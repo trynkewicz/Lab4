9cbf63f5c6d33520eab582565a8a709bb992b926
trynkewicz <71021813+trynkewicz@users.noreply.github.com>
2020-09-11 18:18:46 +0300

    Add files via upload

---
+++ lab4_1.cpp
@@ -0,0 +1,149 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+double* gauss(double** a, double* y, int n)
+{
+	double* x, max;
+	int k, index;
+	const double eps = 0.00001;
+	x = new double[n];
+	k = 0;
+	while (k < n)
+	{
+		max = abs(a[k][k]);
+		index = k;
+		for (int i = k + 1; i < n; i++)
+		{
+			if (abs(a[i][k]) > max)
+			{
+				max = abs(a[i][k]);
+				index = i;
+			}
+		}
+
+		if (max < eps)
+		{
+			return 0;
+		}
+		for (int j = 0; j < n; j++)
+		{
+			double temp = a[k][j];
+			a[k][j] = a[index][j];
+			a[index][j] = temp;
+		}
+		double temp = y[k];
+		y[k] = y[index];
+		y[index] = temp;
+
+		for (int i = k; i < n; i++)
+		{
+			double temp = a[i][k];
+			if (abs(temp) < eps) continue;
+			for (int j = 0; j < n; j++)
+				a[i][j] = a[i][j] / temp;
+			y[i] = y[i] / temp;
+			if (i == k)  continue;
+			for (int j = 0; j < n; j++)
+				a[i][j] = a[i][j] - a[k][j];
+			y[i] = y[i] - y[k];
+		}
+		k++;
+	}
+
+	for (k = n - 1; k >= 0; k--)
+	{
+		x[k] = y[k];
+		for (int i = 0; i < k; i++)
+			y[i] = y[i] - a[i][k] * x[k];
+	}
+	return x;
+}
+
+
+double norma(double l[]) {
+	double sum1 = 0;
+	for (int i = 0; i < 100; i++) {
+		sum1 += fabs(l[i]) * fabs(l[i]);
+	}
+	return sqrt(sum1);
+}
+
+
+int main()
+{
+	double a[100][100] = { 0 };
+	double f[100] = { 0 };
+
+	for (int i = 0; i < 100; i++)
+	{
+		double ai = 1, bi = 10, ci = 1, pi = 1;
+		f[i] = i + 1;
+		if (i > 0 && i < 99)
+		{
+			a[i][i - 1] = ai;
+			a[i][i] = bi;
+			a[i][i + 1] = ci;
+		}
+		else if (i == 0)
+		{
+			a[i][i] = bi;
+			a[i][i + 1] = ci;
+		}
+		else if (i == 99)
+		{
+			for (int j = 0; j < 100; j++)
+			{
+				a[i][j] = pi;
+			}
+		}
+	}
+
+
+
+	double** p, * y, * x;
+	int n = 100;
+
+	p = new double* [n];
+	y = new double[n];
+	for (int i = 0; i < n; i++)
+	{
+		p[i] = new double[n];
+		for (int j = 0; j < n; j++)
+		{
+			p[i][j] = a[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+
+		y[i] = f[i];
+	}
+
+	x = gauss(p, y, n);
+
+	double l[100] = { 0 };
+	double sum;
+	for (int i = 0; i < 100; i++)
+	{
+		sum = 0;
+		for (int j = 0; j < 100; j++)
+		{
+			sum += a[i][j] * x[j];
+		}
+		l[i] = abs(f[i] - sum);
+
+	}
+
+	int i;
+	ofstream out;
+	out.open("ans1.dat");
+	for (i = 0; i < 100; i++) {
+		out << x[i] << endl;
+	}
+	out.close();
+
+	cout << "norma=";
+	cout << norma(l);
+	return 0;
+}
---
+++ lab4_2.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <math.h>
+#include <fstream>
+using namespace std;
+double norma(double l[]) {
+
+	double sum1 = 0;
+	for (int i = 0; i < 100; i++) {
+		sum1 += fabs(l[i]) * fabs(l[i]);
+	}
+	return sqrt(sum1);
+}
+int main()
+{
+
+
+	double tempx[100] = { 0 };
+	double x[100] = { 0 };
+	double a[100][100] = { 0 };
+	double b[100] = { 0 };
+
+	for (int i = 0; i < 100; i++)
+	{
+		double ai = 1, bi = 10, ci = 1, pi = 1;
+		b[i] = i + 1;
+		if (i > 0 && i < 99)
+		{
+			a[i][i - 1] = ai;
+			a[i][i] = bi;
+			a[i][i + 1] = ci;
+		}
+		else if (i == 0)
+		{
+			a[i][i] = bi;
+			a[i][i + 1] = ci;
+		}
+		else if (i == 99)
+		{
+			for (int j = 0; j < 100; j++)
+			{
+				a[i][j] = pi;
+			}
+		}
+	}
+
+
+	int m = 0; 
+	double norm = 0;
+	double eps = 0.0001;
+	double h;
+	do
+	{
+		h = norm;
+		for (int i = 0; i < 100; i++)
+		{
+			x[i] = b[i];
+			for (int j = 0; j < 100; j++)
+			{
+				if (i != j)
+					x[i] -= a[i][j] * x[j];
+			}
+			x[i] /= a[i][i];
+
+		}
+		for (int k = 0; k < 100; k++)
+		{
+			norm += (x[k] - tempx[k]) * (x[k] - tempx[k]);
+			tempx[k] = x[k];
+		}
+		norm = sqrt(norm);
+		m++;
+
+	
+
+	} while (abs(h - norm) > eps);
+
+	cout<<"itera="<<m<<endl;
+
+
+	 
+	double l[100] = { 0 };
+	double sum;
+	for (int i = 0; i < 100; i++)
+	{
+		sum = 0;
+		for (int j = 0; j < 100; j++)
+		{
+			sum += a[i][j] * x[j];
+		}
+		l[i] = abs(b[i] - sum);
+
+	}
+
+	int i;
+	ofstream out;
+	out.open("ans2.dat");
+	for (i = 0; i < 100; i++) {
+		out << x[i] << endl;
+	}
+	out.close();
+	cout << "norma=";
+	cout << norma(l);
+	return 0;
+}
